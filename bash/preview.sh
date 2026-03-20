38851e8550ffc905d95cf8769905269dac2646e0
endoj <elvisindoj@gmail.com>
2026-03-20 14:38:20 +0100

    updated preview.sh script

--- bash/preview.sh
+++ bash/preview.sh
@@ -14,6 +14,7 @@
 SID=( $(cat /etc/oratab | grep -E "\b:" | grep -v "^#" | cut -d: -f1) )
 OH=( $(cat /etc/oratab | grep -E "\b:" | grep -v "^#" | cut -d: -f2) )
 
+ORIG_PATH=$PATH
 
 sql_scripts_path="/home/oracle/oracle-dba/sql"
 shell_scripts_path="/home/oracle/oracle-dba/bash"
@@ -26,7 +27,7 @@ unset ORACLE_SID;
 unset ORACLE_HOME;
 export ORACLE_SID=$1
 export ORACLE_HOME=$(cat /etc/oratab | grep "$ORACLE_SID" | grep -E "\b:" | grep -v "^#" | cut -d: -f2)
-export PATH="$ORACLE_HOME/bin:$PATH"
+export PATH="$ORACLE_HOME/bin:$ORIG_PATH"
 export LD_LIBRARY_PATH=/usr/lib:/usr/lib64:/usr/local/lib:/usr/local/lib64:$ORACLE_HOME/lib
 }
 
